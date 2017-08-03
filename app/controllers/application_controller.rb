8831a6599159fe8f12cc3950535a1217b29998ae
oakley <oakleypeavler@gmail.com>
2017-08-03 10:25:46 -0400

    screwed up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-    private 
   
-      def logged_in?
-        @current_user ||= User.find(session[:user_id]) if session[:user_id]
-      end
-
-      helper_method :logged_in?
+  helper_method :current_user
+ 
+  def current_user
+     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+ 
+  def require_user
+    redirect_to '/login' unless current_user
+  end
 end
